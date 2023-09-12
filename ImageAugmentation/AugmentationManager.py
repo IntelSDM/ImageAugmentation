b432ae332db6ab382b9b0efe40c70c49e33eb6c5
IntelSDM <GitBackup@shibemaster.me>
2023-09-12 16:25:05 +0100

    Fixes For Creating Images

--- ImageAugmentation/AugmentationManager.py
+++ ImageAugmentation/AugmentationManager.py
@@ -3,16 +3,18 @@ import cv2
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
-def MutateFile(string: file, int: amount,index):
-    if index == amount:
+def MutateFile(file, amount,index):
+    if amount == index:
         return
-
-
-
+    image = Image.open("input/" + file)
+    draw = ImageDraw.Draw(image)
+    image.save("output/" +"(" + str(index) + ")" +  file)
     index += 1
+    MutateFile(file,amount,index)
 
 
 
-def MutateImages(files, int: count):
+def MutateImages(files):
+    amount = int(input("Amount Of Images You Want To Augment: "))
     for file in files:
-        MutateFile(file,count,0)
+        MutateFile(file,amount,0)
--- ImageAugmentation/Main.py
+++ ImageAugmentation/Main.py
@@ -1,6 +1,6 @@
 from DataManager import LoadInputs
 from DataManager import Files
-from DataManager import AmountOfImages
+from AugmentationManager import MutateImages
 LoadInputs()
-MutateImages(Files,AmountOfImages)
+MutateImages(Files)
 print("Finished")
